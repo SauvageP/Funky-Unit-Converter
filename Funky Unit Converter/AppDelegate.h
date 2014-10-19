74213e3275bc5797d88444e91d92a507617bcb83
yung305 <prgabriel88@gmail.com>
2014-10-18 21:44:47 -0400

    Initial Commit

---
+++ Funky Unit Converter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Funky Unit Converter
+//
+//  Created by Perry R Gabriel on 10/18/14.
+//  Copyright (c) 2014 Re/Up. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Funky Unit Converter/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Funky Unit Converter
+//
+//  Created by Perry R Gabriel on 10/18/14.
+//  Copyright (c) 2014 Re/Up. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
